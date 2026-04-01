b0222d1ad25b853f1eb37a8b3dd358b3dd0983d0
matiasportugau-ui <matias.portugau@gmail.com>
2026-04-01 04:27:25 -0300

    chore(data): update CALCULATOR_DATA_VERSION_DATE to reflect new timestamp

--- src/data/calculatorDataVersion.js
+++ src/data/calculatorDataVersion.js
@@ -2,4 +2,4 @@
 // Se regenera al ejecutar npm run version:data, npm run dev o npm run build
 
 export const CALCULATOR_DATA_VERSION = "263a036228";
-export const CALCULATOR_DATA_VERSION_DATE = "2026-04-01T03:53:45.670Z";
+export const CALCULATOR_DATA_VERSION_DATE = "2026-04-01T06:40:57.526Z";
