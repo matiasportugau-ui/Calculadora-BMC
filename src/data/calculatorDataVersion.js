7e6b1ae36ff13485ea478011126bfab6192a880a
matiasportugau-ui <matias.portugau@gmail.com>
2026-06-22 11:33:27 -0300

    chore: refresh calculatorDataVersion timestamp after gate build

--- src/data/calculatorDataVersion.js
+++ src/data/calculatorDataVersion.js
@@ -2,4 +2,4 @@
 // Se regenera al ejecutar npm run version:data, npm run dev o npm run build
 
 export const CALCULATOR_DATA_VERSION = "2ad6439a9f";
-export const CALCULATOR_DATA_VERSION_DATE = "2026-06-22T04:15:34.338Z";
+export const CALCULATOR_DATA_VERSION_DATE = "2026-06-22T14:32:20.557Z";
